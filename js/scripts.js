f4906eddb6b13a53f63c3eda7d95368d4ef4e43a
Nina Potrebich <potrebich@gmail.com>
2019-10-25 12:07:34 -0700

    add all forms and logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ $(function() {
             $("#form1").show();
         } else if (answer === "2") {
             $("#form2").show();
+        } else if (answer === "3") {
+            $("#form3").show();
+        } else {
+            $("#form4").show()
         }
     });
 
@@ -266,9 +270,21 @@ $(function() {
         if (answer === "1" || answer === "3") {
             $("#c-sharp").show();
         } else if (answer === "2") {
-            $("#c-plus-plus").show();
+            $("#form2-3-2").show();
         } else {
             $("#form2-3-4").show()
+        }
+    });
+
+    $("#form2-3-2").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp2-3-2]:checked").val();
+        if (answer === "1") {
+            $("#c-plus-plus").show();
+        } else {
+            $("#c-sharp").show()
         }
     });
 
@@ -281,6 +297,140 @@ $(function() {
             $("#c-plus-plus").show();
         } else {
             $("#c-sharp").show()
+        }
+    });
+
+    $("#form3").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp3]:checked").val();
+        if (answer === "1") {
+            $("#form1-3").show();
+        } else if (answer === "2") {
+            $("#form3-2").show();
+        } else if (answer === "3") {
+            $("#form3-3").show();
+        } else if (answer === "4") {
+            $("#form3-4").show();
+        } else {
+            $("#python").show();
+        }
+    });
+
+    $("#form3-2").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp3-2]:checked").val();
+        if (answer === "1" || answer === "2") {
+            $("#html-js").show();
+        } else {
+            $("#form3-2-3").show()
+        }
+    });
+
+    $("#form3-2-3").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp3-2-3]:checked").val();
+        if (answer === "1") {
+            $("#python").show();
+        } else if(answer === "2") {
+            $("#ruby").show();
+        } else {
+            $("#php").show()
+        }
+    });
+
+    $("#form3-3").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp3-3]:checked").val();
+        if (answer === "1" || answer === "3") {
+            $("#html").show();
+        } else {
+            $("#form3-3-2").show()
+        }
+    });
+
+    $("#form3-3-2").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp3-3-2]:checked").val();
+        if (answer === "1") {
+            $("#python").show();
+        } else {
+            $("#c-sharp").show()
+        }
+    });
+
+    $("#form3-4").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp3-4]:checked").val();
+        if (answer === "1") {
+            $("#js").show();
+        } else {
+            $("#form3-4-2").show()
+        }
+    });
+
+    $("#form3-4-2").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp3-4-2]:checked").val();
+        if (answer === "1") {
+            $("#python").show();
+        } else {
+            $("#ruby").show()
+        }
+    });
+
+    $("#form4").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp4]:checked").val();
+        if (answer === "1") {
+            $("#form1-3").show();
+        } else if (answer === "2") {
+            $("#form4-2").show();
+        } else if (answer === "3") {
+            $("#form1-5").show();
+        } else {
+            $("#html").show();
+        }
+    });
+
+    $("#form4-2").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp4-2]:checked").val();
+        if (answer === "1") {
+            $("#html").show();
+        } else {
+            $("#form4-2-2").show()
+        }
+    });
+
+    $("#form4-2-2").submit(function(event) {
+        event.preventDefault();
+        $(".forms").hide();
+        $(".language").hide();
+        var answer = $("input:radio[name=inp4-2-2]:checked").val();
+        if (answer === "1") {
+            $("#php").show();
+        } else if (answer === "2") {
+            $("#form1-2-2-2").show()
+        } else {
+            $("#java").show()
         }
     });
 });
